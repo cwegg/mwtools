ae43176e850dcc0ecc9ade6d2fefd5ae1a1379b9
Chris Wegg <chriswegg+github@gmail.com>
2018-07-24 08:55:05 +0200

    A hopefully sane structure for mwtools

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+setup(name='mwtools',
+      version='0.1',
+      description='Tools for dealing with Milky Way data',
+      author='Chris Wegg',
+      author_email='chriswegg+mwtools@gmail.com',
+      url='https://gitlab.com/chriswegg/mwtools',
+      packages=find_packages(),
+     )
